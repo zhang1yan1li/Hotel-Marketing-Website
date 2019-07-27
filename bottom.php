4c2a20e2fc06a0551382ef09677feaf50eab2ffa
zhang1yan1li <53359107+zhang1yan1li@users.noreply.github.com>
2019-07-27 11:45:23 +0800

    创建项目脚部

---
+++ bottom.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="gb2312" />
+    <title></title>
+    <link rel="stylesheet" href="css/reset.css" />
+<link rel="stylesheet" href="css/footer.css"/>
+</head>
+<body>
+<div>
+<div id="cls">
+    <table align ="center" width="1340">
+        <tr>
+            <td width="72" height="20"><div align="center"><a href="showph.php">销售排行</a></div></td>
+            <td width="72"><div align="center"> <a href="showpl.php">商品评论</a></div></td>
+            <td width="72" height="20"><div align="center"><a href="agreereg.php">本站首页</a></div></td>
+            <td width="72"><div align="center"><a href="showfenlei.php">商品分类</a></div></td>
+            <td width="72" height="20"><div align="center"><a href="agreereg.php">用户注册</a></div></td>
+            <td width="72"><div align="center"><a href="usercenter.php">用户中心</a></div></td>
+            <td width="72"><div align="center"><a href="finddd.php">订单查询</a></div></td>
+            <td width="90"><div align="center"> <a href="shopping.php">我的订单</a></div></td>
+        </tr>
+    </table>
+</div>
+<div class="footer_box">
+    <div class="one">
+        <p><b><a href="index4.html">在线预订</a>&nbsp;<a href="index5.html">在线留言</a></b>&nbsp;&nbsp;品牌背后的力量，才是成功的保证</p>
+        <p>Copyright&nbsp;&copy;&nbsp;2015-2018,www.xxxxx.com,All rights reserved  版权所有 &nbsp;&copy;&nbsp;您的网站名称 未经许可 严禁复制</p>
+        <p>通过宾馆这个窗口，搭起了一座集团公司与外界沟通和传播文化的友谊桥梁</p>
+    </div>
+    <div class="two">
+        <img src="images/logo1.png">
+        <img src="images/logo2.png">
+    </div>
+    <div class="three">
+    </div>
+    <div class="four">
+        <p>订房服务热线</p>
+        <p>400-0000-0000</p>
+    </div>
+</div>
+</div>
+</body>
+</html>
+</body>
+</html>
